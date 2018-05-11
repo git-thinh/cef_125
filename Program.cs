0f35b2d6edd94b2a277c54ddb9e73067ba6ffb44
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-05-12 02:31:19 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace cef_125
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+             
+            if (!CEF.Initialize(new Settings()))
+            {
+                if (Environment.GetCommandLineArgs().Contains("--type=renderer"))
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    return;
+                }
+            } 
+            Application.Run(new BrowserForm());
+        }
+    }
+}
